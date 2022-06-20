7beeaf7c5404d1401eca6955d3aa980f8134cd38
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-06-20 09:52:50 -0500

    Variable de Visualización

---
+++ J2.java
@@ -0,0 +1,15 @@
+// Variables de visualizacion//
+public class J2 {
+    public static void main(String[] args) {
+        String name = "Angel";
+        String Fname = "Leo ";
+        String Lname = "Sol";
+        String FullName = Fname + Lname;
+        int x = 1265;
+        int y = 3214;
+        System.out.println("Hello " + name);
+        System.out.println(FullName);
+        System.out.println(x + y);
+    }
+    
+}
