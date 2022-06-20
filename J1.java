7387613dc0122118e5cf3ac835e8b5142a5da560
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-06-20 09:37:49 -0500

    Variables

---
+++ J1.java
@@ -0,0 +1,17 @@
+// Estructura de java//
+// Variables en java//
+
+public class J1 {
+public static void main(String[] args) {
+    final int myNum = 15;
+    final float Mynum = 15.5f;
+    final char Fnum = 'M';
+    final boolean Snum = true;
+    System.out.println("Hello World");
+    System.out.println(myNum);
+    System.out.println(Mynum);
+    System.out.println(Fnum);
+    System.out.println(Snum);
+}
+}
+
