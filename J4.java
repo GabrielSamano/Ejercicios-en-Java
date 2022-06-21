f9b04bfe87cbd8dd686816283e16c53524e5105d
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-06-21 09:24:46 -0500

    Declaracion If

---
+++ J4.java
@@ -0,0 +1,19 @@
+// Declaracion if//
+public class J4 {
+    public static void main(String[] args) {
+        int time = 21;
+        if (time < 10) {
+            System.out.println("Buen dia");
+        }else {
+            System.out.println("Buena noche");
+        }
+        // Declaracion if y else if//
+        if (time>20) {
+            System.out.println("Buena noche");
+        }else if (time<10){
+            System.out.println("Buen dia");
+        }else{
+            System.out.println("Hola");
+        }
+    }
+}
