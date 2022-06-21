93c32aa86fac6e9014e3a8190ba7429274ffe6f1
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-06-21 09:24:16 -0500

    Booleanos

---
+++ J3.java
@@ -0,0 +1,19 @@
+//Cadenas//
+//Booleanos//
+public class J3 {
+    public static void main(String[] args) {
+        String txt = "Hello World";
+        String Fnma = "David ";
+        String Snma = "Joe";
+        int x = 10;
+        int y = 9;
+        System.out.println(txt);
+        System.out.println("La logintud del texto es " + txt.length());
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+        System.out.println(Fnma.concat(Snma));
+        System.out.println(x < y);
+        System.out.println(x > y);
+        System.out.println(x == y);
+    }
+}
