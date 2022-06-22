796ba25ff96cff003fca50247e2acc9a77369477
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-06-22 09:45:04 -0500

    Bucles

---
+++ J5.java
@@ -0,0 +1,36 @@
+// Switch //
+// Bucle while //
+public class J5 {
+    public static void main(String[] args) {
+        int day = 5;
+        switch (day){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Thursday");
+            break;
+            case 5:
+            System.out.println("Friday");
+            break;
+            case 6:
+            System.out.println("Saturday");
+            break;
+            case 7:
+            System.out.println("Sunday");
+            break;
+
+        }
+        int i = 0;
+        while (i<6) {
+            System.out.println(i);
+            i ++;
+        }
+    }
+}
