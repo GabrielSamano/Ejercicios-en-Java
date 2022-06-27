174be14d70ed526773d2e077a3a96d05162f7a81
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-06-27 10:38:17 -0500

    Clases y Objetos

---
+++ J7.java
@@ -0,0 +1,8 @@
+// Clases y Objetos //
+public class J7 {
+    int x = 15;
+    public static void main(String[] args) {
+        J7 myObjt = new J7();
+        System.out.println(myObjt.x);
+    }
+}
