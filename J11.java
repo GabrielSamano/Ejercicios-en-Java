6c0640b45601d410b8e1a2e66cb9300c2620c1d8
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-07-06 09:16:02 -0500

    Constructor

---
+++ J11.java
@@ -0,0 +1,13 @@
+// Constructor Java //
+public class J11 {
+    int x;  // Crear un atributo de clase
+
+  // Crear un constructor de clase para la clase J11
+  public J11() {
+    x = 5;  
+  }
+  public static void main(String[] args) {
+    J11 myObj = new J11(); // Crear un objeto de la clase J11
+    System.out.println(myObj.x); 
+  }
+}
