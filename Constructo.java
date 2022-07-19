5a409455c52f8955bb88c68ce7b3b2eef2d53c71
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-07-19 19:01:40 +0000

    Ejemplo de Constructor

---
+++ Constructo.java
@@ -0,0 +1,10 @@
+public class Constructo{
+    int x;
+    public Constructo(){
+        x=15;
+    }
+    public static void main(String[] args) {
+        Constructo myObj = new Constructo();
+        System.out.println(myObj.x);
+    }
+}
