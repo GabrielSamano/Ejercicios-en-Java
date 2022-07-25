28579ab3cfc2cf7fc14d6fa7d966e7d538b387fa
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-07-25 15:11:17 +0000

    Linkedlist Java

---
+++ linked.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+public class linked {
+    public static void main(String[] args) {
+        LinkedList<Integer> Nums = new LinkedList<Integer>();
+        Nums.add(12);
+        Nums.add(10);
+        Nums.add(5);
+        System.out.println(Nums);
+    }
+}
