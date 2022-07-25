5e42d3e7d4125d10be950fdaaa383f38435beff1
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-07-25 15:05:11 +0000

    Arraylist con java

---
+++ array.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;;
+public class array {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<String>();
+        fruits.add("apple");
+        fruits.add("Orange");
+        fruits.add("Bannana");
+        fruits.add("Cherry");
+        System.out.println(fruits);
+    }
+}
+
+// Para poder acceder a un elemento se utilizara fruits.get(0 a 3)//
+//eso dependera de que elemto quieres visualizar//
+//Para eliminar se utiliza fruits.remove()//
+// Para cambiar un elemento se utiliza fruits.set()//
+//Para ver el tamaño del array es con fruits.size()//
