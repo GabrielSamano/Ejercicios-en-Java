7f5133a3db424aed51f9d94704e13623258d6e89
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-08-02 18:53:55 +0000

    Matriz con numeros aleatorios

---
+++ MatrizRandom.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+    public class Aleatorio {
+        public static void main(String[] args) {
+
+            Random r=new Random();
+            int[][] a=new int[3][3];
+            for(int i=0;i<3;i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    a[i][j]=r.nextInt(100);
+                    System.out.print(a[i][j]+"\t");
+                }
+
+                System.out.print("\n");
+            }
+
+        }
+}
