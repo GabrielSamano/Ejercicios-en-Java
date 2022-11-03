e8ad4d200015758539253b8447f907e6a64912f1
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-11-03 16:28:52 -0600

    Agregando comentarios

--- MatrizRandom.java
+++ MatrizRandom.java
@@ -3,7 +3,7 @@ import java.util.Random;
         public static void main(String[] args) {
 
             Random r=new Random();
-            int[][] a=new int[3][3];
+            int[][] a=new int[3][3]; /* Se creara una matriz de 3x3 de numeros aleatorios */
             for(int i=0;i<3;i++)
             {
                 for(int j=0;j<3;j++)
