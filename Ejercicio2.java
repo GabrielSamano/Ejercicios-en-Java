15c96df1da0f4f1599ca920da79e65c91ebf4f11
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-11-29 09:34:41 -0600

    Encontrar los caracteres con números

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -4,11 +4,11 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner sn=new Scanner(System.in);
 
-        System.out.println("Introduzca un numero");
+        System.out.println("Introduzca un numero"); // INgresas un numero entero 
 
         int num=sn.nextInt();
 
-        char caracter=(char)num;
+        char caracter=(char)num;  // Los caracteres son asignados por numeros 
 
         System.out.println("El numero " +num+ "corresponde al caracter '" +caracter+ "'");
     }
