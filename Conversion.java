698947f165e3c4cf7df7e1394a3654ba1e581e7b
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2023-01-04 10:31:38 -0600

    Convertidor de tipo de moneda

---
+++ Conversion.java
@@ -0,0 +1,10 @@
+import java.util.Scanner
+// Codigo para convertir el tipo de monedas que hay 
+public class Conversion{
+    Scanner lectro = new Scanner(System.in);
+}
+
+public Conversion{
+    float pDolar, Dolares, cPeso;
+
+}
