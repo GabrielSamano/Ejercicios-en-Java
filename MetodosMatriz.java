e30b5903c8ecc7d0af8c5ed461f0ee227dd5f4fd
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2023-01-23 10:03:40 -0600

    Metodos en una matriz en java

---
+++ MetodosMatriz.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class METODO{
+    public static int apariciones(double matriz [] [], double dato[]){
+        int apariciones = 0;
+        for (int i = 0; i < matriz.lenght; i++ ) {
+            
+        }
+
+    }
+
+}
