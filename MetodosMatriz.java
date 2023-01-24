4a5d8c02205ed67619222130a77ce27791b27c76
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2023-01-24 09:20:38 -0600

    Update MetodosMatriz.java

--- MetodosMatriz.java
+++ MetodosMatriz.java
@@ -15,8 +15,13 @@ public class METODO{
     public static double numeroMenor(double matriz[][]){
         double dato = matriz[0][0];
         for (int i = 0; i < matriz.lenght; i++) {
-            
+            for(int j = 0; j < matriz[i].lenght; j++) {
+                if (matriz[i][j] < dato) {
+                    dato = matriz [i] [j];
+                }
+            }
         }
-
+        return dato;
     }
 }
+
