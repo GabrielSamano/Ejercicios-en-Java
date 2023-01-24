49f554d128be78191fb4bf5a9ef909a947400425
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2023-01-24 08:57:01 -0600

    Update MetodosMatriz.java

--- MetodosMatriz.java
+++ MetodosMatriz.java
@@ -4,9 +4,13 @@ public class METODO{
     public static int apariciones(double matriz [] [], double dato[]){
         int apariciones = 0;
         for (int i = 0; i < matriz.lenght; i++ ) {
-            
+            for (int j = 0; j < matriz[i].lenght; j++) {
+                if (matriz[i][j] == dato) {
+                    apariciones++;
+                }
+            }
         }
-
+        return apariciones;
     }
 
 }
