1ca6f3b73a55db7e16a2c2a2d81f10c1d67526e6
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2023-01-24 09:27:21 -0600

    Update MetodosMatriz.java

--- MetodosMatriz.java
+++ MetodosMatriz.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class METODO{
-    public static int apariciones(double matriz [] [], double dato[]){
+    public static int apariciones(double matriz [] [], double dato[]){  /*"La matriz sera minimo de dos elementos"*/
         int apariciones = 0;
         for (int i = 0; i < matriz.lenght; i++ ) {
             for (int j = 0; j < matriz[i].lenght; j++) {
